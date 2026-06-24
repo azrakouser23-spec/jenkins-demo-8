e35c2eea694d15dedea437341d0f458f74c545b1
azrakouser <azrakouser23@gmail.com>
2026-06-24 12:37:45 +0530

    fixed main class issue

--- src/main/java/org/example/app.java
+++ src/main/java/org/example/app.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class app {
     public static void main(String[] args){
-        System.out.println("hello jenkins");
+        System.out.println("hello Jenkins");
     }
 }
